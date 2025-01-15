d391da181abef7264d7f39642f74647abdf59e98
sajnabhandari <123648542+sajnabhandari@users.noreply.github.com>
2025-01-15 02:22:17 +0000

    update1

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("Welcome to the mental health awareness Chatbot!")
 name = input("What is your name, user? ")
+print("Nice to meet you " + name + "!")
+age = input("How old are you " + name +" ?")
