3f8a2da91a008d9309d11ef7edd3bbb3e231daaa
sajnabhandari <123648542+sajnabhandari@users.noreply.github.com>
2025-01-16 02:10:55 +0000

    update2-useroptions?

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-print("Welcome to the mental health awareness Chatbot!")
+print("Welcome to the menu chatbot!")
 name = input("What is your name, user? ")
 print("Nice to meet you " + name + "!")
 age = input("How old are you " + name +" ?")
+
 while True:
-    print("How can I help you today? ")
-    print("1) option 1")    
-    print("2) option 2")    
-    print("3) option 3")    
-    print("4) option 4")    
-    userChoice = input("Please choose from the list of options shown above (ex: 1)")
+    print("\nHow can I help you today?")
+    print("1) Option 1")    
+    print("2) Option 2")    
+    print("3) Option 3")    
+    print("4) Option 4")    
+    print("Exit) Exit")
+    #
+    userChoice = input("Please choose from the list of options shown above or exit (ex: 1): ")
+    ##
     if userChoice == "1":
-    print("option1")
+        print("Option 1 selected")
     elif userChoice == "2":
-    print("option2")
+        print("Option 2 selected")
     elif userChoice == "3":
-    print("option3")
+        print("Option 3 selected")
     elif userChoice == "4":
-    print("option4")
+        print("Option 4 selected")
+    elif userChoice.lower() == "exit":
+        print("Exiting... Goodbye!")
+        break
     else:
-     print("Oops, invalid choice. Could you pick from what is listed above?")
+        print("Oops, invalid choice. Could you pick from the options listed above?")
+#print("This is what " + name + " has chosen so far")
