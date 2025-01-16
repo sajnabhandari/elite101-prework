e903a638899f9efc318de7829f6c6ab7fe628f73
sajnabhandari <123648542+sajnabhandari@users.noreply.github.com>
2025-01-16 02:09:30 +0000

    chnge1

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ while True:
     print("3) option 3")    
     print("4) option 4")    
     userChoice = input("Please choose from the list of options shown above (ex: 1)")
-if userChoice == "1":
+    if userChoice == "1":
     print("option1")
-elif userChoice == "2":
+    elif userChoice == "2":
     print("option2")
-elif userChoice == "3":
+    elif userChoice == "3":
     print("option3")
-elif userChoice == "4":
+    elif userChoice == "4":
     print("option4")
-else:
-    print("Oops, invalid choice. Could you pick from what is listed above?")
+    else:
+     print("Oops, invalid choice. Could you pick from what is listed above?")
