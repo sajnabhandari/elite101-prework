5a8799185e5f226ba425f33b7f74848bbd30a18d
sajnabhandari <123648542+sajnabhandari@users.noreply.github.com>
2025-02-04 00:16:37 +0000

    retailstore-format&changes w/options

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-print("Welcome to the menu chatbot!")
-name = input("What is your name, user? ")
-print("Nice to meet you " + name + "!")
-age = input("How old are you " + name +" ?")
+print("Welcome to the Return and Exchange Assistant!")
+print("This chatbot will help you with returning or exchanging your items quickly and easily.")
 
+name = input("Please enter your name: ")
+phone_number = input("Please enter the phone number you made your order with: ")
+
+print(f"Thank you, {name}! Let's get started with your request.")
 while True:
     print("\nHow can I help you today?")
-    print("1) Option 1")    
-    print("2) Option 2")    
-    print("3) Option 3")    
-    print("4) Option 4")    
+    print("1) Exchange Item")
+    print("2) Return Item")
+    print("3) Exchange and Return History")
     print("Exit) Exit")
-    #print("done")
-    #
+
     userChoice = input("Please choose from the list of options shown above or exit (ex: 1): ")
-    ##
+
     if userChoice == "1":
-        print("Option 1 selected")
+        print("Option 1 selected: Exchange Item")
     elif userChoice == "2":
-        print("Option 2 selected")
+        print("Option 2 selected: Return Item")
     elif userChoice == "3":
-        print("Option 3 selected")
-    elif userChoice == "4":
-        print("Option 4 selected")
+        print("Option 3 selected: Exchange and Return History")
     elif userChoice.lower() == "exit":
         print("Exiting... Goodbye!")
         break
     else:
         print("Oops, invalid choice. Could you pick from the options listed above?")
-#print("This is what " + name + " has chosen so far")
-#print("Total cost: " + totalcost)
-#checkout=input("Would you like to check out?")
